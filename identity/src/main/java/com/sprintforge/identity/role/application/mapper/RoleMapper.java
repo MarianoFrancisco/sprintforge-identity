56b2d9841d1c72292ea68a0fa687d976e0b8193c
Mariano Camposeco <mariano1941@outlook.es>
2025-12-09 16:06:21 -0600

    feat: add role mapper to domain

---
+++ identity/src/main/java/com/sprintforge/identity/role/application/mapper/RoleMapper.java
@@ -0,0 +1,18 @@
+package com.sprintforge.identity.role.application.mapper;
+
+import com.sprintforge.identity.role.application.port.in.command.CreateRoleCommand;
+import com.sprintforge.identity.role.domain.Role;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RoleMapper {
+    public Role toDomain(
+            CreateRoleCommand command
+    ) {
+        return new Role(
+                command.name(),
+                command.description(),
+                command.isDefault()
+        );
+    }
+}
