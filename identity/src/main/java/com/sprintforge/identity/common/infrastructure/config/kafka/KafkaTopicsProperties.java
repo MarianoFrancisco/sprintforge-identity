2df28469e7fa6ca754aaf946732ca4e94a67067a
Mariano Camposeco <mariano1941@outlook.es>
2025-12-11 19:05:31 -0600

    feat: add kafka properties

---
+++ identity/src/main/java/com/sprintforge/identity/common/infrastructure/config/kafka/KafkaTopicsProperties.java
@@ -0,0 +1,14 @@
+package com.sprintforge.identity.common.infrastructure.config.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "spring.kafka.topics")
+public class KafkaTopicsProperties {
+    String employeeCreated;
+}
