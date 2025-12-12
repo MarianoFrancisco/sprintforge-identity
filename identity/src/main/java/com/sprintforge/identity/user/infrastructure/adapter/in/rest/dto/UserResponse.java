f5077843c30b3cf6422b94f4cc9112d50b4d3358
Mariano Camposeco <mariano1941@outlook.es>
2025-12-11 18:05:39 -0600

    feat: add user dto and rest mapper

---
+++ identity/src/main/java/com/sprintforge/identity/user/infrastructure/adapter/in/rest/dto/UserResponse.java
@@ -0,0 +1,4 @@
+package com.sprintforge.identity.user.infrastructure.adapter.in.rest.dto;
+
+public class UserResponse {
+}
---
+++ identity/src/main/java/com/sprintforge/identity/user/infrastructure/adapter/in/rest/mapper/UserRestMapper.java
@@ -0,0 +1,7 @@
+package com.sprintforge.identity.user.infrastructure.adapter.in.rest.mapper;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserRestMapper {
+}
