b94327cac3e7abe32f8e06dd64b735dbadcdd559
Mariano Camposeco <mariano1941@outlook.es>
2025-12-11 19:06:23 -0600

    feat: add user exceptions

---
+++ identity/src/main/java/com/sprintforge/identity/user/application/exception/UserNotFoundException.java
@@ -0,0 +1,24 @@
+package com.sprintforge.identity.user.application.exception;
+
+import com.sprintforge.identity.common.application.exception.EntityNotFoundException;
+
+import java.util.UUID;
+
+public class UserNotFoundException extends EntityNotFoundException {
+
+    private UserNotFoundException(String field, String value) {
+        super(String.format("No se encontró ningún usuario con %s \"%s\".", field, value));
+    }
+
+    public static UserNotFoundException byId(UUID id) {
+        return new UserNotFoundException("el identificador", id.toString());
+    }
+
+    public static UserNotFoundException byUsername(String username) {
+        return new UserNotFoundException("el nombre de usuario", username);
+    }
+
+    public static UserNotFoundException byEmail(String email) {
+        return new UserNotFoundException("el correo electrónico", email);
+    }
+}
