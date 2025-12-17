ca763b025ce6e37cbc1b01890f97a6431b9d3fc9
Mariano Camposeco <mariano1941@outlook.es>
2025-12-16 18:06:20 -0600

    refactor: modify permission

--- identity/src/main/java/com/sprintforge/identity/permission/infrastructure/adapter/out/persistence/mapper/PermissionEntityMapper.java
+++ identity/src/main/java/com/sprintforge/identity/permission/infrastructure/adapter/out/persistence/mapper/PermissionEntityMapper.java
@@ -7,21 +7,33 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class PermissionEntityMapper {
     public Permission toDomain(PermissionEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+
         return new Permission(
                 entity.getId(),
                 entity.getCode(),
                 entity.getName(),
-                entity.getDescription(),
+                entity.getDescription() != null ? entity.getDescription() : null,
                 entity.getCategory()
         );
     }
 
     public PermissionEntity toEntity(Permission domain) {
+        if (domain == null) {
+            return null;
+        }
+
+        String description = domain.getDescription() != null
+                ? domain.getDescription().value()
+                : null;
+
         return PermissionEntity.builder()
                 .id(domain.getId().value())
                 .code(domain.getCode().value())
                 .name(domain.getName().value())
-                .description(domain.getDescription().value())
+                .description(description)
                 .category(domain.getCategory().value())
                 .build();
     }
