9ed279c2717733828d297b3a11bf1ea72e35d928
Mariano Camposeco <mariano1941@outlook.es>
2025-12-16 18:04:59 -0600

    feat: add new structure of kafka

--- identity/src/main/java/com/sprintforge/identity/common/infrastructure/config/kafka/KafkaGroupsProperties.java
+++ identity/src/main/java/com/sprintforge/identity/common/infrastructure/config/kafka/KafkaGroupsProperties.java
@@ -2,13 +2,9 @@ package com.sprintforge.identity.common.infrastructure.config.kafka;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Component
-@ConfigurationProperties(prefix = "spring.kafka.groups")
 public class KafkaGroupsProperties {
     private String defaultGroup;
 }
--- identity/src/main/java/com/sprintforge/identity/common/infrastructure/config/kafka/KafkaTopicsProperties.java
+++ identity/src/main/java/com/sprintforge/identity/common/infrastructure/config/kafka/KafkaTopicsProperties.java
@@ -2,13 +2,9 @@ package com.sprintforge.identity.common.infrastructure.config.kafka;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Component
-@ConfigurationProperties(prefix = "spring.kafka.topics")
 public class KafkaTopicsProperties {
-    String employeeCreated;
+    private String employeeCreated;
 }
