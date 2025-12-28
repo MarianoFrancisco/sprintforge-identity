f20d37dd8052377ee45e4ae2d90beffeb90f2ad7
Mariano Camposeco <mariano1941@outlook.es>
2025-12-28 16:37:15 -0600

    feat: add general role out persitence port

---
+++ identity/src/main/java/com/sprintforge/identity/role/application/port/out/persistence/LoadRoleGeneralReport.java
@@ -0,0 +1,7 @@
+package com.sprintforge.identity.role.application.port.out.persistence;
+
+import com.sprintforge.identity.role.application.port.result.RoleGeneralReportResult;
+
+public interface LoadRoleGeneralReport {
+    RoleGeneralReportResult loadRoleGeneralReport();
+}
